28ff633c00f022d0dcd5d57fe300e4f7193163fd
NgoMike <mqngo21@gmail.com>
2018-02-07 17:31:30 -0800

    Almost done with helper tests

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -26,16 +26,77 @@ exports.initialize = function(pathsObj) {
 // modularize your code. Keep it clean!
 
 exports.readListOfUrls = function(callback) {
+  // return urls in archives/sites.text
+  fs.readFile(exports.paths.list, (err, data) => {
+    // checking if there's an error
+    if (err) {
+      // if yes, throw error
+      throw err;
+    }
+    // use template literal to convert hex code to a string, then split on '\n'
+    let urls = `${data}`.split('\n');
+    // invoke callback on err and urls
+    callback(err, urls);
+  });
 };
 
 exports.isUrlInList = function(url, callback) {
+  // checking if url is on the list
+  fs.readFile(exports.paths.list, (err, data) => {
+    // checking if there's an error
+    if (err) {
+      // if yes, throw error
+      throw err;
+    }
+    // use template literal to convert hex code to a string, then split on '\n'
+    let urls = `${data}`.split('\n');
+    // check if url is in urls array
+    let bool = urls.includes(url);
+    // invoke callback on err and boolean
+    callback(err, bool);
+  });
 };
 
 exports.addUrlToList = function(url, callback) {
+  // adding a url to the list
+  // check if url is in list
+  this.isUrlInList(url, (err, data) => {
+    // if error, throw error
+    if (err) {
+      throw err;
+    }
+    // if url is not yet in list already
+    if (data === false) {
+      // add url to list
+      fs.writeFile(exports.paths.list, url, (err) => {
+        // if error, throw error
+        if (err) {
+          throw err;
+        }
+        callback(err);
+      }); 
+    }
+  });
 };
 
 exports.isUrlArchived = function(url, callback) {
+  // checking if url is archived
+  fs.readdir(exports.paths.archivedSites, (err, data) => {
+    // checking if there's an error
+    if (err) {
+      // if yes, throw error
+      throw err;
+    }
+    // use template literal to convert hex code to a string, then split on '\n'
+    let urls = `${data}`;
+    console.log(urls);
+    // check if url is in urls array
+    let bool = urls.includes(url);
+    // invoke callback on err and boolean
+    callback(err, bool);
+  });
 };
 
 exports.downloadUrls = function(urls) {
+  // 
 };
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,27 @@
 var path = require('path');
+var fs = require('fs');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+exports.handleRequest = function (request, response) {
+
+
+/*
+  // set GET method
+  if (request.method === 'GET' && request.url === '/') {
+    // write head {content-type: app/json}
+    // end of response 
+    fs.readFile('./public/index.html', (err, data) => {
+      console.log("content:", data);
+      if (err) {
+        throw err;
+      }
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.end(data);
+    });
+  }
+  // set POST method
+  response.end(archive.paths.list);
+
+*/
 };
